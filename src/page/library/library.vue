307b76133954151436ab49a8d3b4a68511223ad3
machengcheng <616254086@qq.com>
2017-12-19 23:09:30 +0800

    修改了书库

--- src/page/library/library.vue
+++ src/page/library/library.vue
@@ -1,42 +1,47 @@
 <template>
   <div>
-    <!-- <ReadHeader/> -->
     <head-bar head_index='true' tab_value='library'></head-bar>
-    <mu-bottom-nav :value="bottomNav"  @change="handleChange">
-      <mu-bottom-nav-item value="people" title="人气" icon="favorite"/>
-      <mu-bottom-nav-item value="favorites" title="时间" icon="restore"/>
-      <mu-bottom-nav-item value="nearby" title="字数" icon="pie_chart"/>
-      <mu-bottom-nav-item value="recents" title="订阅" icon="turned_in"/>
-      <mu-bottom-nav-item value="more" title="更多" icon="more_vert"/>
-      <mu-drawer right :open="open" :docked="docked" @close="toggle()" width="90%">
-        <mu-sub-header id="head-title"><b>筛选</b></mu-sub-header>
-        <hr/>
-        <div v-for="(tags, subject) in filterTags">
-          <mu-sub-header class="subject-title"><b>{{subject}}</b></mu-sub-header>
-          <mu-row>
-            <mu-col v-for="(tagState, tagName) in tags" width="25" tablet="25" desktop="25">
-              <mu-chip @click="selectFilter(subject, tagName)" v-bind:class="[tagState?'r-tag-selected':'r-tag-disselected']">
-                {{tagName}}
-              </mu-chip>
-            </mu-col>
-          </mu-row>
-          <mu-divider/>
-        </div>
+    <mu-flexbox class="nav-bar">
+        <mu-flexbox-item v-for="navTitle in navTitles">
+            <div v-on:click="handleChange(navTitle.icon)" v-bind:class="[navTitle.icon==activeNav?'nav-bar-active':'']">
+                <i class="material-icons">{{navTitle.icon}}</i>
+                <span>
+                    {{navTitle.name}}
+                </span>
+            </div>
+        </mu-flexbox-item>
 
-        <div id="r-bottom-drawer">
-          <mu-flat-button backgroundColor="#f6f7f9" class="r-bottom-button" label="重置" @click="resetFilter"/>
-          <mu-raised-button class="r-bottom-button" label="完成" primary @click="finshSet"/>
-        </div>
-      </mu-drawer>
-    </mu-bottom-nav>
+        <!-- 书籍筛选 -->
+        <mu-drawer right :open="open" :docked="docked" @close="toggle()" width="90%">
+            <h3 class="drawer-title">筛选</h3>
+          <div v-for="(tags, subject) in filterTags">
+            <h4 class="sub-title">{{subject}}</h4>
+            <mu-row>
+              <mu-col v-for="(tagState, tagName) in tags" width="25" tablet="25" desktop="25">
+                <div @click="selectFilter(subject, tagName)" v-bind:class="[tagState?'tag-selected':'tag-disselected']">
+                  {{tagName}}
+                </div>
+              </mu-col>
+            </mu-row>
+            <mu-divider/>
+          </div>
+          <div class="bottom-drawer">
+            <mu-flat-button backgroundColor="#f6f7f9" class="bottom-button" label="重置" @click="resetFilter"/>
+            <mu-raised-button class="bottom-button" label="完成" primary @click="finshSet"/>
+          </div>
+        </mu-drawer>
+    </mu-flexbox>
+
     <mu-divider/>
 
+    <!-- 书籍列表 -->
     <mu-list>
          <mu-list-item　v-for="bookItem in bookList" to="/book/">
             <book-item :book="bookItem"></book-item>
         </mu-list-item>
     </mu-list>
     <mu-infinite-scroll :scroller="scroller" :loading="loading" @load="loadMore" loadingText="猫猫为您加载中"/>
+
   </div>
 </template>
 
@@ -51,15 +56,20 @@ export default {
     bookItem,
   },
   data () {
+    const navTitles = [
+        {name: '人气', icon: 'favorite'},
+        {name: '时间', icon: 'restore'},
+        {name: '字数', icon: 'pie_chart'},
+        {name: '订阅', icon: 'turned_in'},
+        {name: '更多', icon: 'more_vert'},
+    ]
 
     const filterTags = {
-      '类型': {'仙剑': false, '玄幻': false, '悬疑': false, '奇幻': false, '军事': false,
-              '历史': false, '竞技': false, '科幻': false, '校园': false, '社会': false,
-              '其它': false, '': false},
-      '属性': {'免费': false, 'VIP': false, '': false, '': false},
-      '字数': {'30万以下': false, '30~50万': false, '50~100万': false, '100~200万': false,
-              '200万以上': false},
-      '状态': {'连载': false, '完结': false, '': false, '': false},
+      '类型': {'仙剑': false, '玄幻': false, '悬疑': false, '奇幻': false,
+              '军事': false, '历史': false, '竞技': false, '科幻': false,
+              '校园': false, '社会': false, '其它': false, '': null},
+      '属性': {'免费': false, 'VIP': false, '': null, '': null},
+      '状态': {'连载': false, '完结': false, '': null, '': null},
       '更新时间': {'三日内': false, '七日内': false, '半月内': false, '一月内': false}
     }
 
@@ -72,8 +82,11 @@ export default {
     }
 
     return {
+      navTitles,
       filterTags,
       selectedTags,
+
+      activeNav: 'favorite',
 
       bookList: [],
       numPage: 1,
@@ -100,26 +113,24 @@ export default {
     },
     handleChange (tag) {
       switch (tag) {
-        case 'more':
+        case 'more_vert':
           this.open = !this.open
           this.docked = false
           break;
-          this.bottomNav = tag
         default:
-          console.log(tag);
+          this.activeNav =tag;
       }
     },
     toggle () {
       this.open = !this.open
     },
     loadMore () {
-      this.loading = true
-      // this.numPage += 1
-      setTimeout(() => {
+        this.loading = true
+        setTimeout(() => {
         this.getBookList()
         this.num += 10
         this.loading = false
-    }, 1500)
+        }, 1500)
     },
     getBookList () {
       let bookList;
@@ -163,17 +174,22 @@ export default {
 
       let  index = getIndex()
 
-      if (index == this.selectedTags[subject][1]) {
-        this.filterTags[subject][tagName] = false;
-        this.selectedTags[subject][1] = 0;
-      }else {
-        if (0 != this.selectedTags[subject][1]) {
-          this.filterTags[subject][getTagName(this.selectedTags[subject][1])] = false;
-        }
-        this.filterTags[subject][tagName] = true;
-        this.selectedTags[subject][1] = index;
+      // 对非空项进行筛选
+      if (null != this.filterTags[subject][tagName]){
+          //  对已选择重复点击情况,取消选中
+          if (index == this.selectedTags[subject][1]) {
+            this.filterTags[subject][tagName] = false;
+            this.selectedTags[subject][1] = 0;
+          }else {
+              // 对未选择的情况,更换选择状态
+              // 若已选择的情况,清空之前的选择
+              if (0 != this.selectedTags[subject][1]) {
+                  this.filterTags[subject][getTagName(this.selectedTags[subject][1])] = false;
+              }
+              this.filterTags[subject][tagName] = true;
+              this.selectedTags[subject][1] = index;
+          }
       }
-
     },
     resetFilter(){
       for (let index in this.selectedTags) {
@@ -192,45 +208,69 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 
-
-  #head-title{
-    color: #000000;
-    font-size: 20px;
-  }
-
-  .subject-title{
-    color: #000000;
-  }
-
-  .r-tag-selected{
-    background-color: #fffff;
-    height: 25px;
-    margin-bottom: 15px;
-  }
-
-  .r-tag-disselected{
-    background-color: #ffffff;
-    height: 25px;
-    margin-bottom: 15px;
-  }
-
-  .head_book_bottom{
-    font-size: 12px;
-  }
-
-  .head_book-type{
-    float: right;
-    color: rgba(0,0,0,.54);
+  .tag-selected{
+    margin: 0 8px 0 8px;
     border: 1px solid #ddd;
+    border-radius: 10px;
   }
 
-  #r-bottom-drawer{
-    margin-top: 60px;
+  .tag-disselected{
+    margin: 0 8px 0 8px;
+    background-color: #ffffff;
+    height: 35px;
+    margin-bottom: 15px;
   }
 
-  .r-bottom-button{
-    width: 49%;
+  .bottom-drawer{
+    position: sticky;
+    padding-top: 50px;
+    bottom: 0px;
+    width: 100%;
+    .bottom-button{
+        width: 49%;
+    }
   }
-</style>
+
+  .nav-bar{
+      text-align: center;
+      color: #969ba3;
+      padding-top: 5px;
+      i{
+          font-size: 1.4rem;
+      }
+      span{
+          display: block;
+      }
+
+  }
+
+  .nav-bar-active{
+      color: #ff4081;
+  }
+
+  .drawer-title{
+      text-align: center;
+      line-height: 48px;
+      display: inline;
+      padding: 0 16px 0 16px;
+  }
+
+  .drawer-title::after{
+      content: "";
+      height: 1px;
+      position: absolute;
+      top: 48px;
+      left: 0;
+      right: 0;
+      background-color: #eee;
+      box-shadow: 0 1px 6px rgba(0,0,0,.117647), 0 1px 4px rgba(0,0,0,.117647);
+  }
+
+  .sub-title{
+      line-height: 38px;
+      display: inline;
+      padding: 0 16px 0 16px;
+  }
+  </style>
